e5922b45c7eef0401279d40642b1acfcb9d03060
terentyevtsv <terentyevtsv@ya.ru>
2020-07-31 10:41:00 +0400

    Добавил конфигурации сборки

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// Библиотека для получения пути с учетом кроссплатформенности
+const path = require('path');
+
+module.exports = {
+  mode: 'development',                      // Режим сборки для разработки
+  entry: './src/main.js',                   // Точка входа
+  output: {
+    filename: 'bundle.js',                  // Файл сборки
+    path: path.join(__dirname, 'public'),   // Место расположения сборки (абсолютный путь до директории public)
+  },
+  devtool: 'source-map'                     // Генерация source-map для быстрой навигации в файле сборки
+};
