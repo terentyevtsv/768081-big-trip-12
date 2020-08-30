d808858225313a183dd72f9bbc33fd36b843a1b0
terentyevtsv <terentyevtsv@ya.ru>
2020-08-30 17:00:40 +0400

    Добавил модули для работы с диаграммами. Закинул реализацию диаграмм как есть.

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,4 +1,8 @@
 import AbstractView from "./abstract.js";
+import Chart from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const BAR_HEIGHT = 55;
 
 const createStatisticsTemplate = () =>
   `<section class="statistics">
@@ -20,9 +24,161 @@ const createStatisticsTemplate = () =>
 export default class Statistics extends AbstractView {
   constructor() {
     super();
+
+    this._moneyChart = this._renderMoneyChart();
+    this._transportChart = this._renderTransportChart();
   }
 
   getTemplate() {
     return createStatisticsTemplate();
+  }
+
+  _renderMoneyChart() {
+    const moneyCtx = this.getElement().querySelector(`.statistics__chart--money`);
+    moneyCtx.height = BAR_HEIGHT * 6;
+
+    const moneyChart = new Chart(moneyCtx, {
+      plugins: [ChartDataLabels],
+      type: `horizontalBar`,
+      data: {
+        labels: [`✈️ FLY`, `???? STAY`, `???? DRIVE`, `????️ LOOK`, `???? EAT`, `???? RIDE`],
+        datasets: [{
+          data: [400, 300, 200, 160, 150, 100],
+          backgroundColor: `#ffffff`,
+          hoverBackgroundColor: `#ffffff`,
+          anchor: `start`
+        }]
+      },
+      options: {
+        plugins: {
+          datalabels: {
+            font: {
+              size: 13
+            },
+            color: `#000000`,
+            anchor: `end`,
+            align: `start`,
+            formatter: (val) => `€ ${val}`
+          }
+        },
+        title: {
+          display: true,
+          text: `MONEY`,
+          fontColor: `#000000`,
+          fontSize: 23,
+          position: `left`
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: `#000000`,
+              padding: 5,
+              fontSize: 13,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            barThickness: 44,
+          }],
+          xAxes: [{
+            ticks: {
+              display: false,
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            minBarLength: 50
+          }],
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false,
+        }
+      }
+    });
+
+    return moneyChart;
+  }
+
+  _renderTransportChart() {
+    const transportCtx = this.getElement().querySelector(`.statistics__chart--transport`);
+    transportCtx.height = BAR_HEIGHT * 4;
+
+    const transportChart = new Chart(transportCtx, {
+      plugins: [ChartDataLabels],
+      type: `horizontalBar`,
+      data: {
+        labels: [`???? DRIVE`, `???? RIDE`, `✈️ FLY`, `????️ SAIL`],
+        datasets: [{
+          data: [4, 3, 2, 1],
+          backgroundColor: `#ffffff`,
+          hoverBackgroundColor: `#ffffff`,
+          anchor: `start`
+        }]
+      },
+      options: {
+        plugins: {
+          datalabels: {
+            font: {
+              size: 13
+            },
+            color: `#000000`,
+            anchor: `end`,
+            align: `start`,
+            formatter: (val) => `${val}x`
+          }
+        },
+        title: {
+          display: true,
+          text: `TRANSPORT`,
+          fontColor: `#000000`,
+          fontSize: 23,
+          position: `left`
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: `#000000`,
+              padding: 5,
+              fontSize: 13,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            barThickness: 44,
+          }],
+          xAxes: [{
+            ticks: {
+              display: false,
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            minBarLength: 50
+          }],
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false,
+        }
+      }
+    });
+
+    return transportChart;
+  }
+
+  _renderTimeSpendChart() {
+    const timeSpendCtx = this.getElement().querySelector(`.statistics__item--time-spend`);
+    timeSpendCtx.height = BAR_HEIGHT * 4;
   }
 }
